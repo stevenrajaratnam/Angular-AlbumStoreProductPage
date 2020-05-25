015aad57604e979bba063d69ed70edccfeabd82f
Steven Rajaratnam <stevenrajaratnam@hotmail.com>
2020-05-25 13:24:38 +0100

    part 3 with http fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient  } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
