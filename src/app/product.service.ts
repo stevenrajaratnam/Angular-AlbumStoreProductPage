63c9ad0fa7bee74c865bade7df7cbcdd2f24501e
Steven Rajaratnam <stevenrajaratnam@hotmail.com>
2020-05-25 18:15:08 +0100

    part 6 fix

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -19,6 +19,6 @@ export class ProductService {
    }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map((response) => <Product[]>response);
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
    }
 }
